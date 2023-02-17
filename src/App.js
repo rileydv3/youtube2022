fc959bcdf3cbbc6e4efab4102cc5b9f233d851d6
Denny <rileydv@gmail.com>
2023-02-17 06:09:46 -0500

    Setting up node and react.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
